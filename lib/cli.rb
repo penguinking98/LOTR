135f42d106e3e3c58abe25fc3493b26b93f1dc61
Ben Kramer <benkramer614@gmail.com>
2021-04-01 19:47:03 -0400

    Cli fixes

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,7 +10,6 @@ class CLI
         prompt_user_narrow_by_gender
         prompt_user_narrow_by_race
         prompt_user_narrow_by_name
-        #prompt_user_narrow_by_realm
         show_matches   
         thanks
     end
@@ -79,21 +78,7 @@ class CLI
 
     end
     
-    #def prompt_user_narrow_by_realm
-        #puts "Please select from the following ages."
-        #realm = @pool.collect {|character| character.realm}.uniq     
-        #realm.each_with_index{|age, i| puts "\t#{i+1}. #{realm}" }
-        #ask users to type in race number
-       # input = Readline.readline("> ", true)
-       # choice = realm[input.to_i-1]
-       # puts "You picked : #{choice}" 
-    #    #narrow pool by race
-      #  @pool.delete_if {|character| character.realm == choice}
-         # Tell user how many characters in pool now
-       #  puts "Character pool is now #{@pool.size} characters."
-    
-   # end
-    
+       
     def show_matches
         puts "The following #{@pool.size} characters match your criteria."
         @pool.each {|character| puts "\t#{character}"}
@@ -101,20 +86,10 @@ class CLI
     def thanks
     puts "Thank you for using my Lord of the Rings app!"
     end
-    #def_valid_input
-    #end
+   
 end
 
-
-cli = CLI.new
-cli.greeting 
-cli.fetch_character_pool 
-cli.prompt_user_narrow_by_gender
-cli.prompt_user_narrow_by_race
-cli.prompt_user_narrow_by_name
-#cli.prompt_user_narrow_by_realm
-cli.show_matches   
-cli.thanks
+    CLI.new.call
 
 
 #q to quit or r to return to the beginning of the app
